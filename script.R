d02e1b052431b944fd0dc5d7a117d31899064410
unknown <saulomorellato@gmail.com>
2024-03-20 08:32:32 -0300

    updating project

---
+++ script.R
@@ -0,0 +1,17 @@
+## PACOTES ##
+
+library(ISLR)       # dataset Auto
+library(tidyverse)	# manipulacao de dados
+library(tidymodels) # ferramentas de ML
+library(stacks)     # stacking
+library(tictoc)     # registrar o tempo de execução de comandos
+#library(mixOmics)
+#library(plsmod)
+
+
+
+## DADOS ##
+
+df<- Auto
+glimpse(df)
+
